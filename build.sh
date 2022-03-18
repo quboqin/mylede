da1ab023187fd262b7d2b9343b32a94f380796e5
root <root@X86.localdomain>
2022-03-18 13:06:32 +0800

    build x86 on WSL

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+startTime=`date +%Y%m%d-%H:%M` 
+startTime_s=`date +%s`
+ 
+make -j1 V=s
+ 
+endTime=`date +%Y%m%d-%H:%M`
+endTime_s=`date +%s`
+sumTime=$[ $endTime_s - $startTime_s ]
+ 
+echo "$startTime ---> $endTime" "Totl:$sumTime minutes" 
